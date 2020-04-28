8476142cd47273a63b3deac28c2a5d28380af2a9
seatedSinger <18754154+seatedSinger@users.noreply.github.com>
2020-04-28 23:26:52 +0530

    inorder

--- traversal_tree.py
+++ traversal_tree.py
@@ -1,7 +1,20 @@
-def inOrder(root):
-    stack, res = [], []
-    node = root
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-    while stack and node:
-        while stack:
-            
+    def inOrder(self,root):
+        stack, res = [], []
+        node = root
+
+        while stack and node:
+            while node:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            res.append(node.val)
+            node = node.right
+        return res
+
