1928ff79f47f8a38122bf7134e9179a82fa2bf35
seatedSinger <18754154+seatedSinger@users.noreply.github.com>
2020-05-11 14:22:32 +0530

    BST

---
+++ BST_del.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self, key):
+        self.val = val
+        self.left = left
+        self.right = right
--- traversal_tree.py
+++ traversal_tree.py
@@ -18,3 +18,4 @@ class TreeNode:
             node = node.right
         return res
 
+#PreOrder
