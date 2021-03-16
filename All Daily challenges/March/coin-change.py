99340f72e04262fd5c9efdce1a475ea74a8d26a3
seatedSinger <18754154+seatedSinger@users.noreply.github.com>
2021-03-16 23:05:33 +0530

    toss-a-coin

--- All Daily challenges/March/coin-change.py
+++ All Daily challenges/March/coin-change.py
@@ -1,22 +1,22 @@
-# F(n) = F(n-1) + F(n-3) + F(n-5) + F(n-10)
+# O(n * k), n = no. of coins | k = amount of money
+'''
+F(n) = F(n-1) + F(n-3) + F(n-5) + F(n-10)
+'''
 
 def coinChange(coins, amount):
-    dp = [0] + [float('inf') for i in range(amount)]
+    dp = [float('inf') for i in range(amount+1)]
+    dp[0] = 0
     for i in range(1, amount+1):
         for coin in coins:
             if i - coin >= 0:
                 dp[i] = min(dp[i], dp[i-coin] + 1)
     if dp[-1] == float('inf'):
         return -1
+    return dp,dp[-1]
 
-def second(coins, amount):
-    dp1 = [0] + [float('inf') for i in range(amount)]
-    for i in range(1, amount + 2):
-        for coin in coins:
-            if i - coin >= 0:
-                pass
+def solution2(coins, amount):
+    pass
 
 #TODO : Add bottom up | Top down variations
-
 
 print(coinChange([1,2,5],11))
