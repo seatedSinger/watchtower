2af5a1f54ea3c28f981a62d5177f23f2f2de12fe
seatedSinger <18754154+seatedSinger@users.noreply.github.com>
2021-04-29 22:22:39 +0530

    Stock I, II, with Fee

--- All Daily challenges/March/buy-sell-stock - 2.py
+++ All Daily challenges/March/buy-sell-stock - 2.py
@@ -7,10 +7,11 @@ def maxProfit(prices):
     res = 0
     # ans  = []
     for i in range(1, len(prices)):
-        res += max(0,prices[i] - prices[i-1])
+        res += max(0, prices[i] - prices[i-1])
         # ans.append((prices[i] - prices[i-1]))
     return res
     # print(ans)
+
 
 def solution2(prices):
     # max_profit = 0
@@ -25,6 +26,15 @@ def solution2(prices):
     return sum(res)
 
 
-a = [7,1,5,3,6,4]
-print(maxProfit(a))
-print(solution2(a))
+def maxProfitFee(prices, fee):
+    minSoFar, res = float('inf'), 0
+    for price in prices:
+        minSoFar = min(minSoFar, price - res)
+        res = max(res, price - minSoFar - fee)
+    return res
+
+
+# a = [7,1,5,3,6,4]\
+# print(maxProfit(a))
+# print(solution2(a))
+print(maxProfitFee([1, 3, 7, 5, 10, 3], 3))
