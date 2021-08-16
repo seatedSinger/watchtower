8ab013f6e93388d7904a1e51c67dd2b262072449
seatedSinger <18754154+seatedSinger@users.noreply.github.com>
2021-08-16 22:54:01 +0530

    Slide

---
+++ Weekly 254/Subarray Product less than K.py
@@ -0,0 +1,12 @@
+nums = [10, 5, 2, 6]
+k = 100
+
+
+def productLessThanK(nums, k):
+    prod = 1
+    left, right = 0, 0
+    for i in range(len(nums)):
+        pass
+
+
+print(productLessThanK(nums, 100))
