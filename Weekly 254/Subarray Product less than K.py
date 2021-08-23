5d13a61eced688dbfa51dc4da80b68aba4607e71
seatedSinger <18754154+seatedSinger@users.noreply.github.com>
2021-08-23 23:08:15 +0530

    Subarray_prod

--- Weekly 254/Subarray Product less than K.py
+++ Weekly 254/Subarray Product less than K.py
@@ -3,10 +3,15 @@ k = 100
 
 
 def productLessThanK(nums, k):
-    prod = 1
-    left, right = 0, 0
-    for i in range(len(nums)):
-        pass
+    left, prod, res = 0, 1, 0
+    # for i, r in enumerate(nums):
+    for right in range(len(nums)):
+        prod *= nums[right]
+        while prod >= k and left <= right:
+            prod /= nums[left]
+            left += 1
+        res += right - left + 1
+    return res
 
 
 print(productLessThanK(nums, 100))
