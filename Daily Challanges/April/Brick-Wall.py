9f70ce2f25c34d6ed3e0896ebcb7da0f3f3abf91
seatedSinger <18754154+seatedSinger@users.noreply.github.com>
2021-09-06 17:33:00 +0530

    Brick-Hash

--- Daily Challanges/April/Brick-Wall.py
+++ Daily Challanges/April/Brick-Wall.py
@@ -1,6 +1,10 @@
 '''
-Line is Optimal HOW ?
 Find Edge with highest frequency in the wall
+Number of Bricks Crossed by Line = Number of Rows in Wall - Frequency of Most Occuring Edge
+So:
+Number of Bricks Crossed by Line = Number of Rows in Wall - Frequency of Most Occuring Edge
+=> Number of Bricks Crossed by Line = 6 - 4
+=> Number of Bricks Crossed by Line = 2 (We return this answer)
 '''
 from collections import defaultdict
 
@@ -10,6 +14,10 @@ wall = [[1, 2, 2, 1],
         [2, 4],
         [3, 1, 2],
         [1, 3, 1, 1]]
+
+wall2 = [[1],
+         [1],
+         [1]]
 
 
 def wallAndBricks(wall):
@@ -27,4 +35,17 @@ def wallAndBricks(wall):
     return len(wall) - max_edges
 
 
+def solution2(wall):
+    freq = {}
+    for bricks in wall:
+        bricksEndPoint = 0
+        for i in range(len(bricks) - 1):
+            bricksEndPoint += bricks[i]
+            freq[bricksEndPoint] = freq.get(bricksEndPoint, 0) + 1
+    count = freq.values()
+    return len(wall) - (max(count) if count else 0)
+
+
 print(wallAndBricks(wall))
+print(solution2(wall))
+print(solution2(wall2))
