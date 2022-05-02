e8b7a6b2165abc9d6d4cbf094aa860831251ddab
seatedSinger <18754154+seatedSinger@users.noreply.github.com>
2022-05-02 19:01:15 +0530

    Hard

---
+++ Contests/Weekly/Weekly 291 - MAY/Total Appeal of A String.py
@@ -0,0 +1,37 @@
+'''
+The appeal of a string is the number of distinct characters found in the string.
+
+For example, the appeal of "abbca" is 3 because it has 3 distinct characters: 'a', 'b', and 'c'.
+Given a string s, return the total appeal of all of its substrings.
+
+A substring is a contiguous sequence of characters within a string.
+
+============================================================
+
+Input: s = "abbca"
+Output: 28
+Explanation: The following are the substrings of "abbca":
+- Substrings of length 1: "a", "b", "b", "c", "a" have an appeal of 1, 1, 1, 1, and 1 respectively. The sum is 5.
+- Substrings of length 2: "ab", "bb", "bc", "ca" have an appeal of 2, 1, 2, and 2 respectively. The sum is 7.
+- Substrings of length 3: "abb", "bbc", "bca" have an appeal of 2, 2, and 3 respectively. The sum is 7.
+- Substrings of length 4: "abbc", "bbca" have an appeal of 3 and 3 respectively. The sum is 6.
+- Substrings of length 5: "abbca" has an appeal of 3. The sum is 3.
+The total sum is 5 + 7 + 7 + 6 + 3 = 28.
+'''
+
+def appealSum(s):
+    prev = {}
+    total, curr = 0, 0
+    for i, c in enumerate(s):
+        if c in prev:
+            curr += i + 1 - (prev[c])
+            prev[c] = (i + 1)
+        else:
+            prev[c] = (i+1)
+            curr += i + 1
+        total += curr
+    return total
+
+
+print(appealSum("abbca"))
+print(appealSum("code"))
